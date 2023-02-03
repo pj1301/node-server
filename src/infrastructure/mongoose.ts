92f45108a0d15bc2ab179d50d8bf7fb747b392a2
pj1301 <hayward.jp@gmail.com>
2023-02-03 08:50:06 +0000

    updated mongoose config and removed public serve

--- src/infrastructure/mongoose.ts
+++ src/infrastructure/mongoose.ts
@@ -1,11 +1,27 @@
-import { connect, set } from 'mongoose';
+import { connect, set, plugin, Schema } from 'mongoose';
 
 import { logger } from '../lib';
 
 export function dBInit() {
 	logger.info('SERVER INITIALISATION::Mongoose Database Driver');
+
+	function setValidators(this: any) {
+		this.setOptions({ runValidators: true });
+	}
+
 	try {
 		set('strictQuery', false);
+		/*
+		Update operations do not run
+		validators by default, they
+		must be enabled manually.
+		*/
+		plugin((schema: Schema) => {
+			schema.pre('findOneAndUpdate', setValidators);
+			schema.pre('updateOne', setValidators);
+			schema.pre('updateMany', setValidators);
+		});
+
 		connect(process.env.MONGO_URL as string);
 	} catch (e) {
 		logger.error(e);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,8 +15,7 @@ function configureRoutes(server: Application) {
 	*/
 	server.use('/api/v1/auth', authRouter).use('/api/v1/users', userRouter);
 
-	server.use('/public', express.static(path.join(__dirname, '../../public')));
-	/*
+	/* UNCOMMENT BELOW TO SERVE UI
 	UI ROUTES
 	the ui will be served when
 	accessing the root url with
@@ -28,11 +27,12 @@ function configureRoutes(server: Application) {
 	bundle them in with the
 	bundled javascript
 	*/
-	server
-		.use(express.static(PATH))
-		.get('/*.js', (req, res) => res.sendFile(`${PATH}/index.js`))
-		.get('/*.js.map', (req, res) => res.sendFile(`${PATH}/index.js.map`))
-		.get('/*', (req, res) => res.sendFile(`${PATH}/index.html`));
+	// server.use('/public', express.static(path.join(__dirname, '../../public')));
+	// server
+	// 	.use(express.static(PATH))
+	// 	.get('/*.js', (req, res) => res.sendFile(`${PATH}/index.js`))
+	// 	.get('/*.js.map', (req, res) => res.sendFile(`${PATH}/index.js.map`))
+	// 	.get('/*', (req, res) => res.sendFile(`${PATH}/index.html`));
 }
 
 export { configureRoutes };
